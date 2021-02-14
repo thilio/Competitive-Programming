a1cde2ed917bced491184d41e54d4fb3d50409d9
Thiago Lima Oliveira <thilio@usp.br>
2021-02-13 23:16:56 -0300

    Suffix Array

---
+++ Notebook/String/sa.cpp
@@ -0,0 +1,94 @@
+//Tested in CodeForces, Kattis, Yosupo;
+//TODO: Test in tight problems : Average common prefix - Timus
+//TODO: Upgrade and test LCP with RMQ 
+
+#include "bits/stdc++.h"
+using namespace std;
+const int MAXN = 100100;
+
+int n;
+int p[MAXN], c[MAXN], lcp[MAXN], cnt[MAXN], p_new[MAXN], c_new[MAXN];
+pair <char,int> a[MAXN];
+
+void count_sort(){
+	fill(cnt, cnt + n + 1, 0);
+
+	for(int i = 0; i < n; i++) cnt[c[i] + 1]++;
+
+	cnt[0] = 0;
+	for(int i = 1; i < n; i++){
+		cnt[i] += cnt[i-1];
+	}
+
+	for(int i =0; i < n; i++){
+		int j = c[p[i]];
+		p_new[cnt[j]] = p[i];
+		cnt[j]++;
+	}
+
+	for(int i = 0; i < n; i++){
+		p[i] = p_new[i];
+	}
+}
+
+void build(string &s){ // Build the SA in p, and the LCP in the array lcp. Complexity O(n logn)
+	s.push_back('$'); // REMEMBER YOU ARE MODIFYING THE STRING
+	n = s.size();
+
+	for(int i = 0; i < n; i++) a[i] = {s[i],i};
+	sort(a, a + n);
+
+	p[0] = n - 1;
+	c[p[0]] = 0;
+
+
+
+	for(int i = 1; i < n; i++){
+		p[i] = a[i].second;
+		c[p[i]] = c[p[i - 1]] + (a[i].first != a[i - 1].first);
+	}
+
+
+	int k = 0;
+	while((1 << k) < n){
+		for(int i = 0; i < n; i++){
+			p[i] = (p[i] - (1 << k) + n)%n;
+		}
+
+		count_sort();
+
+		c_new[p[0]] = 0;
+		for(int i = 1; i < n; i++){
+			pair<int,int> at = {c[p[i]], c[(p[i] + (1 << k))%n]};
+			pair<int,int> prev = {c[p[i-1]], c[(p[i-1] + (1 << k))%n]};
+			c_new[p[i]] = c_new[p[i-1]] + (at != prev);
+		}
+		for(int i =0; i < n; i++) c[i] = c_new[i];
+		k++;
+		
+	}
+
+	k = 0;
+	for(int i = 0; i < n-1; i++){
+		int pi = c[i];
+		int j = p[pi - 1];
+		while(s[i + k] == s[j + k]) k++;
+
+		lcp[pi] = k;
+		k = max(0,k - 1);
+	}
+}
+
+int main(){
+
+	string t;
+	getline(cin,t);
+	build(t);
+
+	n = t.size();
+	for(int i = 0; i < n; i++){
+		cout << p[i] << ' ' << endl;
+	}
+
+
+}
