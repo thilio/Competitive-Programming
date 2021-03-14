55d675fb82dae549fa83cd30efcf619f9b2a7300
Thiago Lima Oliveira <thilio@usp.br>
2021-03-14 18:37:42 -0300

    Update code to a better version

--- OJ/Yosupo/Number of Substrings.cpp
+++ OJ/Yosupo/Number of Substrings.cpp
@@ -28,30 +28,37 @@ const ll llINF = 0x3f3f3f3f3f3f3f;
 const int MOD = 1e9+7;
 
 const int N = 2000000;
+const int NS = 1;
 ll ans;
-string s;
-int n;
 int en = 1;
 int ns;
+string S[NS];
 
 struct node{
-	int l,r;
-	int p, suf;
+	int l, r, p;
+	int si, suf;
 	map<char,int> f;
-	node(): l(0), r(-1) {}
-	node(int L, int R, int P): l(L), r(R), p(P) {}
-	inline int len() {return r -l + 1;}
-	inline char operator[](int i){return s[i + l];}
+	node(): l(0), r(-1), p(0), si(0) {}
+	node(int L, int R, int P, int S): l(L), r(R), p(P), si(S) {}
+	inline int len() {return r - l + 1;}
+	inline char operator[](int i){return S[si][i + l];} // CHANGE OPERATOR WHEN USING INTEGERS
 }t[N];
 
-inline int new_node(int L, int R, int P){
-	t[en] = node(L, R, P);
+inline int new_node(int L, int R, int P, int S){
+	t[en] = node(L, R, P, S);
+	t[en].f.clear();
 	return en++;
 }
 
-void build(){
-	s.push_back('$');
-	n = s.size();
+void clear(){
+	en = 1;
+	t[0].f.clear();
+}
+
+void build(string &s, int k){
+	s.push_back('!' + k);
+	S[k] = s;
+	int n = s.size();
 	int i,cn,cd;
 	i = cn = cd = 0;
 	for(int j = 0; j < n; j++){
@@ -66,16 +73,16 @@ void build(){
 			}
 
 			if(cd == t[cn].len()){
-				t[cn].f[s[j]] = new_node(j, n - 1, cn);
+				t[cn].f[s[j]] = new_node(j, n - 1, cn, k);
 				if(cn){
 					cn = t[cn].suf;
 					cd = t[cn].len();
 				}
 			}
 			else{
-				int mid = new_node(t[cn].l, t[cn].l + cd - 1, t[cn].p);
+				int mid = new_node(t[cn].l, t[cn].l + cd - 1, t[cn].p, t[cn].si);
 				t[t[mid].p].f[t[mid][0]] = mid;
-				t[mid].f[s[j]] = new_node(j, n -1, mid);
+				t[mid].f[s[j]] = new_node(j, n -1, mid, k);
 				t[mid].f[t[cn][cd]] = cn;
 				t[cn].p = mid;
 				t[cn].l += cd;
@@ -120,8 +127,10 @@ void dfs(int cn){
 int main(){
 
 	fastio;
+	string s;
 	cin >> s;
-	build();
+	build(s, 0);
 	dfs(0);
+	int n = S[0].size();
 	cout << ans - n<< endl;
 }
